4ac0d748741178ef834b13b32aaa3dcb4208f2db
Arifice <arifice13@gmail.com>
2023-09-16 16:45:47 +0600

    Components folder is created and Header Component is also created and Header section is done.

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <div className="p-8">
+            <h1 className='text-5xl text-center font-bold'>Course Registration</h1>
+        </div>
+    );
+};
+export default Header;
