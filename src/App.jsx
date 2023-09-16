6ddef50c53807eef03e5004a54ca6d40fb3a1cdd
Arifice <arifice13@gmail.com>
2023-09-16 16:37:44 +0600

    project create and tailwind Css file are downloded and then run

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Course from './Components/Course/Course'
+import Header from './Components/Header/Header'
+function App() {  
+  return (
+    <>      
+      <Header></Header> 
+      <Course></Course>     
+    </>
+  )
+}
+export default App
